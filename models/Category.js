3e6c5949c4724f561870ee7120b5af55fd984bb9
thechicketarian <thechicketarian@gmail.com>
2022-06-15 22:27:37 -0500

    Updating Models

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,17 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      AutoIncrement: true,
+    },
+      category_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,30 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      AutoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      }
+
+    }
+
+
+    //
   },
   {
     sequelize,
